833704a7f991493ca0408e62536791aa164c0b70
JES20 <dore.joel.dore@gmail.com>
2021-02-09 07:19:50 -0500

    Update cache names and service worker fetching

--- client/service-worker.js
+++ client/service-worker.js
@@ -9,53 +9,81 @@ const FILES_TO_CACHE = [
     'https://cdn.jsdelivr.net/npm/chart.js@2.8.0'
 ];
 
-const PRECACHE = 'precache-v1';
-const RUNTIME = 'runtime';
+const STATIC_CACHE = 'static-cache-v1';
+const DATA_CACHE = "data-cache-v1";
 
 self.addEventListener('install', (event) => {
+    // pre cache transaction data
     event.waitUntil(
         caches
-            .open(PRECACHE)
-            .then((cache) => cache.addAll(FILES_TO_CACHE))
-            .then(self.skipWaiting())
+            .open(DATA_CACHE)
+            .then((cache) => cache.add("/api/transaction"))
     );
+
+    // pre cache static assets
+    event.waitUntil(
+        caches
+            .open(STATIC_CACHE)
+            .then((cache) => cache.addAll(FILES_TO_CACHE))
+    );
+    // activate service worker once installed
+    self.skipWaiting()
 });
 
 self.addEventListener('activate', (event) => {
-    const currentCaches = [PRECACHE, RUNTIME];
+    const currentCaches = [DATA_CACHE, STATIC_CACHE];
+
     event.waitUntil(
-        caches
-            .keys()
-            .then((cacheNames) => {
-                return cacheNames.filter((cacheName) => !currentCaches.includes(cacheName));
-            })
-            .then((cachesToDelete) => {
-                return Promise.all(
-                    cachesToDelete.map((cacheToDelete) => {
-                        return caches.delete(cacheToDelete);
-                    })
-                );
-            })
-            .then(() => self.clients.claim())
+        caches.keys().then(keyList => {
+            return Promise.all(
+                keyList.map(key => {
+                    if (!currentCaches.includes(key)) {
+                        console.log("Removing old cache data", key);
+                        return caches.delete(key);
+                    }
+                })
+            );
+        })
     );
+
+    self.clients.claim();
 });
 
 self.addEventListener('fetch', (event) => {
-    if (event.request.url.startsWith(self.location.origin)) {
-        event.respondWith(
-            caches.match(event.request).then((cachedResponse) => {
-                if (cachedResponse) {
-                    return cachedResponse;
-                }
 
-                return caches.open(RUNTIME).then((cache) => {
-                    return fetch(event.request).then((response) => {
-                        return cache.put(event.request, response.clone()).then(() => {
+    if (event.request.url.includes("/api/transaction")) {
+        event.respondWith(
+            caches
+                .open(DATA_CACHE)
+                .then(cache => {
+                    return fetch(event.request)
+                        .then(response => {
+                            // If the response was good, clone it and store it in the cache.
+                            if (response.status === 200) {
+                                cache.put(event.request.url, response.clone());
+                            }
                             return response;
+                        })
+                        .catch(err => {
+                            // Network request failed, try to get it from the cache.
+                            return cache.match(event.request);
                         });
-                    });
-                });
-            })
+                })
+                .catch(err => console.log(err))
         );
+        return
     }
+
+    // if the request is not for the API, serve static assets using "offline-first" approach.
+    // see https://developers.google.com/web/fundamentals/instant-and-offline/offline-cookbook#cache-falling-back-to-network
+    event.respondWith(
+        caches
+            .open(STATIC_CACHE)
+            .then(cache => {
+                return cache.match(event.request)
+                    .then((response) => {
+                        return response || fetch(event.request);
+                    })
+            })
+    );
 });
